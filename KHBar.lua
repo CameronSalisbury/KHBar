560214de6f8e2a2068b6f6ec5381b0deed752cd3
CameronSalisbury <elitreys@gmail.com>
2024-03-30 22:31:51 -0500

    updated KHbar mod

--- KHBar.lua
+++ KHBar.lua
@@ -7,6 +7,7 @@ SlashCmdList.RELOADUI = ReloadUI
 
 --------------------------------------------------------------------------------------------------
 --[[
+--This will work on its own. just displays the art i made	
 local UIConfig = CreateFrame("Frame", ShowUF, UIParent)
 
 local KHBar = UIConfig:CreateTexture()
@@ -14,16 +15,22 @@ KHBar:SetTexture("Interface\\Addons\\KHBar\\media\\KHPspritez")
 KHBar:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT")
 KHBar:SetSize(512,286)
 UIConfig.texture = KHBar
-]]
+--]]
+
+-- hide the old health bar
+PlayerFrame:Hide()
+
+
 
 local Health = CreateFrame("StatusBar", "hiuiPlayerHealthBar", self)
 --[[        Player Health Border (Art)
             Art frame keeping the health bar in check.
 --]]
+
 local HealthBorder = Health:CreateTexture("hiuiPlayerHealthArt", "ARTWORK", nil, 1)
-HealthBorder:SetTexture(defaults.player.textures.border)
-HealthBorder:SetTexCoord(0, 377/512, 0, 60/64)
-HealthBorder:SetPoint("TOPLEFT", self, "TOPLEFT")
+HealthBorder:SetTexture("Interface\\Addons\\KHBar\\media\\KHPspritez")
+HealthBorder:SetTexCoord(0, 512/512, 0, 286/286)
+--HealthBorder:SetPoint("TOPLEFT", self, "TOPLEFT")	-- Commented out because im confused on how this texcord command works
 HealthBorder:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT")
 
 
@@ -31,7 +38,7 @@ HealthBorder:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT")
             Dark bar that moves when you take damage.
 --]]
 local mask = Health:CreateMaskTexture()
-mask:SetTexture("Interface\\Addons\\KHBar\\media\\HealthbarMasked")
+mask:SetTexture("Interface\\Addons\\KHBar\\media\\HealthbarMask1", "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
 mask:SetPoint("TOPLEFT", Health, "TOPLEFT", 0, 0)
 mask:SetPoint("BOTTOMRIGHT", Health, "BOTTOMRIGHT", 0, 0)
  
@@ -76,72 +83,3 @@ do -- bad practice, code abuse, programming under the influence
 
     C_Timer.After(0.8, fixInnerElementsPos)
 end
-
---[[
-local function CreateBar(name, previous) -- Create StatusBar with a text overlay
-	local f = CreateFrame("StatusBar", "Fizzle"..name, UIParent)
-	f:SetSize(300, 30)
-	if not previous then
-		f:SetPoint("BOTTOMRIGHT", -100, 200)
-	else
-		f:SetPoint("TOP", previous, "BOTTOM")
-	end
-	f:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-	f.Text = f:CreateFontString()
-	f.Text:SetFontObject(GameFontNormal)
-	f.Text:SetPoint("CENTER")
-	f.Text:SetJustifyH("CENTER")
-	f.Text:SetJustifyV("CENTER")
-	return f
-end
-
-local function UpdateHealth(self, unit) -- Update the health bar
-	local health = UnitHealth(unit)
-	self:SetValue(health)
-	self.Text:SetText(FormatLargeNumber(health) .. "/" .. FormatLargeNumber(self.healthMax))
-end
-local function UpdatePower(self, unit) -- Update the power bar
-	local power = UnitPower(unit)
-	self:SetValue(power)
-	self.Text:SetText(FormatLargeNumber(power) .. "/" .. FormatLargeNumber(self.powerMax))
-end
-local function UpdateHealthMax(self, unit) -- Update min./max. health values
-	self.healthMax = UnitHealthMax(unit)
-	self:SetMinMaxValues(0, self.healthMax)
-	UpdateHealth(self, unit)
-end
-local function UpdatePowerMax(self, unit) -- Update min./max. power values
-	self.powerMax = UnitPowerMax(unit)
-	self:SetMinMaxValues(0, self.powerMax)
-	UpdatePower(self, unit)
-end
-local HealthBar = CreateBar("PlayerHealth") -- Create the health bar
-HealthBar:SetStatusBarColor(0, 1, 0) -- make it green
-local PowerBar = CreateBar("PlayerPower", HealthBar) -- Create the power bar (set to anchor below Health)
-PowerBar:SetStatusBarColor(0, 0, 1) -- make it blue
-HealthBar:SetScript("OnEvent", function(self, event, ...)
-	local unit = ... -- For events starting with UNIT_ the first parameter is the unit
-	if unit ~= "player" then  -- We"re only updating the player status ATM
-		return -- So ignore any other unit
-	end
-	if event == "UNIT_HEALTH_FREQUENT" then -- Fired when health changes
-		UpdateHealth(self, unit)
-	elseif event == "UNIT_POWER_FREQUENT" then -- Fired when power changes
-		UpdatePower(PowerBar, unit)
-	elseif event == "UNIT_MAXHEALTH" then -- Fired when max. health changes
-		UpdateHealthMax(self, unit)
-	elseif event == "UNIT_MAXPOWER" then -- Fired when max. power changes
-		UpdatePowerMax(PowerBar, unit)
-	end
-end)
-
-
-UpdateHealthMax(HealthBar, "player") -- initialise the health bar for Player health
-UpdatePowerMax(PowerBar, "player") -- initialise the power bar for Player power
-HealthBar:RegisterEvent("UNIT_HEALTH") -- register the events to be used (UNIT_HEALTH_FREQUENT only exists in old versions these days
-HealthBar:RegisterEvent("UNIT_POWER_FREQUENT") -- Health bar is handling events for both bars
-HealthBar:RegisterEvent("UNIT_MAXHEALTH")
-HealthBar:RegisterEvent("UNIT_MAXPOWER")
-
-]]
-
